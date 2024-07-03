4816c552de103043f650c8d3e7ea06b6cfc9ea4d
JunHyeok Yoon <jhy230912@gmail.com>
2024-07-03 19:05:52 +0900

    add Check & Delete

--- main.js
+++ main.js
@@ -4,24 +4,66 @@ const $taskBoard = document.getElementById("taskBoard");
 const taskList = [];
 
 const addTask = () => {
-  const taskContent = $taskInput.value;
-  taskList.push(taskContent);
+  const task = {
+    id: createRandomID(),
+    taskContent: $taskInput.value,
+    isComplete: false,
+  };
+  taskList.push(task);
   render();
 };
 
 const render = () => {
   let resultHTML = "";
+
   for (let i = 0; i < taskList.length; i++) {
-    resultHTML += `<div id="taskItem" class="task-item">
-            <div>${taskList[i]}</div>
+    if (taskList[i].isComplete == true) {
+      resultHTML += `<div id="taskItem" class="task-item task-item-complete">
+            <div class="task-complete">${taskList[i].taskContent}</div>
             <div>
-              <button>Check</button>
-              <button>Delete</button>
+              <i class="fa-solid fa-rotate-left task-button return-button" onclick="completeTask('${taskList[i].id}')"></i>
+              <i class="fa-regular fa-square-minus task-button delete-button" onclick="deleteTask('${taskList[i].id}')"></i>
+            </div>
+          </div>`;
+      continue;
+    }
+
+    resultHTML += `<div id="taskItem" class="task-item">
+            <div>${taskList[i].taskContent}</div>
+            <div>
+              <i class="fa-solid fa-check task-button check-button" onclick="completeTask('${taskList[i].id}')"></i>
+              <i class="fa-regular fa-square-minus task-button delete-button" onclick="deleteTask('${taskList[i].id}')"></i>
             </div>
           </div>`;
   }
   $taskBoard.innerHTML = resultHTML;
 };
 
+const completeTask = (id) => {
+  for (i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
+    }
+  }
+  render();
+};
+
+const deleteTask = (id) => {
+  for (i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList.splice(i, 1);
+      break;
+    }
+  }
+  render();
+};
+
+const createRandomID = () => {
+  return Math.random().toString(36).substr(2, 9);
+};
+
 $addButton.addEventListener("click", addTask);
-$taskInput.addEventListener("focus", function(){$taskInput.value="";});
+$taskInput.addEventListener("focus", function () {
+  $taskInput.value = "";
+});

bb22e0d5eddfe7cac0889f56db26f5f19a1fc410
JunHyeok Yoon <jhy230912@gmail.com>
2024-07-04 21:22:47 +0900

    big update

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 const $taskInput = document.getElementById("taskInput");
 const $addButton = document.getElementById("addButton");
 const $taskBoard = document.getElementById("taskBoard");
+const tabs = document.querySelectorAll(".task-tabs div");
 const taskList = [];
+let filterList = [];
+let mode = "all";
 
 const addTask = () => {
+  if ($taskInput.value == "") {
+    alert("비어있는 값은 입력할 수 없습니다");
+    return;
+  } else {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].taskContent == $taskInput.value) {
+        alert("중복된 값은 입력할 수 없습니다");
+        return;
+      }
+    }
+  }
   const task = {
     id: createRandomID(),
     taskContent: $taskInput.value,
@@ -11,28 +25,37 @@ const addTask = () => {
   };
   taskList.push(task);
   render();
+  $taskInput.value = "";
 };
 
 const render = () => {
+  let list = [];
+
+  if (mode === "all") {
+    list = taskList;
+  } else if (mode === "ongoing" || mode === "done") {
+    list = filterList;
+  }
+
   let resultHTML = "";
 
-  for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].isComplete == true) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete == true) {
       resultHTML += `<div id="taskItem" class="task-item task-item-complete">
-            <div class="task-complete">${taskList[i].taskContent}</div>
+            <div class="task-complete">${list[i].taskContent}</div>
             <div>
-              <i class="fa-solid fa-rotate-left task-button return-button" onclick="completeTask('${taskList[i].id}')"></i>
-              <i class="fa-regular fa-square-minus task-button delete-button" onclick="deleteTask('${taskList[i].id}')"></i>
+              <i class="fa-solid fa-rotate-left task-button return-button" onclick="completeTask('${list[i].id}')"></i>
+              <i class="fa-regular fa-square-minus task-button delete-button" onclick="deleteTask('${list[i].id}')"></i>
             </div>
           </div>`;
       continue;
     }
 
     resultHTML += `<div id="taskItem" class="task-item">
-            <div>${taskList[i].taskContent}</div>
+            <div>${list[i].taskContent}</div>
             <div>
-              <i class="fa-solid fa-check task-button check-button" onclick="completeTask('${taskList[i].id}')"></i>
-              <i class="fa-regular fa-square-minus task-button delete-button" onclick="deleteTask('${taskList[i].id}')"></i>
+              <i class="fa-solid fa-check task-button check-button" onclick="completeTask('${list[i].id}')"></i>
+              <i class="fa-regular fa-square-minus task-button delete-button" onclick="deleteTask('${list[i].id}')"></i>
             </div>
           </div>`;
   }
@@ -46,7 +69,8 @@ const completeTask = (id) => {
       break;
     }
   }
-  render();
+  
+  document.getElementById(mode).click();
 };
 
 const deleteTask = (id) => {
@@ -56,7 +80,42 @@ const deleteTask = (id) => {
       break;
     }
   }
+  for (let i = 0; i < filterList.length; i++) {
+    if (filterList[i].id == id) {
+      filterList.splice(i, 1);
+      break;
+    }
+  }
   render();
+};
+
+const filter = (event) => {
+  mode = event.target.id;
+  filterList = [];
+  if (mode === "all") {
+    render();
+  } else if (mode === "ongoing") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete == false) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  } else if (mode === "done") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete == true) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  }
+};
+
+const indicator = (event) => {
+  tabs[0].style.left = event.currentTarget.offsetLeft + "px";
+  tabs[0].style.width = event.currentTarget.offsetWidth + "px";
+  tabs[0].style.top =
+    event.currentTarget.offsetTop + event.currentTarget.offsetHeight - 3 + "px";
 };
 
 const createRandomID = () => {
@@ -64,6 +123,17 @@ const createRandomID = () => {
 };
 
 $addButton.addEventListener("click", addTask);
-$taskInput.addEventListener("focus", function () {
-  $taskInput.value = "";
+$taskInput.addEventListener("keyup", (event) => {
+  if (event.keyCode === 13) {
+    addTask();
+  }
 });
+
+for (let i = 1; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function (event) {
+    filter(event);
+  });
+  tabs[i].addEventListener("click", function (event) {
+    indicator(event);
+  });
+}
